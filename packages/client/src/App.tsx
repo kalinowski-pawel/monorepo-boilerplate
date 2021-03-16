b0712b69d6d03e70d1679e47a1bd1c0fdd224e72
Pawel Kalinowski <pkalinowski83@gmail.com>
2021-03-16 11:36:21 +0100

    removed unneeded files and ts added

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+
+      <header className="App-header">Hello monorepo</header>
+    </div>
+  );
+}
+
+export default App;
