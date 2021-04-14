28d2ceb192900089663111b5c62eabc8b7c12b19
Paweł Kalinowski <pkalinowski83@gmail.com>
2021-04-14 19:52:24 +0200

    Storybook basic configuration (#3)
    * storybook basic configuration
    
    * readme updated

---
+++ packages/library/src/components/button/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MatButton from "@material-ui/core/Button";
+
+const Button = ({type, disabled, children}) =>
+  <MatButton
+    variant="contained"
+    color={type}
+    disabled={disabled}>
+    {children}
+  </MatButton>
+export default Button;
